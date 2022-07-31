a509bddb798a4049910a7b0d7845477b5f9f1556
Sandxy <110335821+Sandxy@users.noreply.github.com>
2022-07-31 15:34:11 +0200

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,40 @@
+
+
+
+function txthover(articleNR) {
+
+let text;
+var number = articleNR;
+let description;
+
+switch (number) {
+    case 2:
+        text = "After the brutal murder of his fiancé, he turns the killers live into a living nightmare. But he made one big mistake: he underestimated the killer and soon enough the game gets turned around.";
+        description = "description2";
+        break;
+    case 3:
+        text = "After the brutal murder of his fiancé, he turns the killers live ns the killers live s the killers live s the killers live into a living nightmare. But he made one big mistake: he underestimated the killer and soon enough the game gets turned around.";
+        description = "description3";
+        break;
+    case 4:
+        text = "After the brutal murder of ut he made one big mistake: he underestimated the killer and soon enough the game gets turned around.";
+        description = "description4";
+        break;
+    case 5:
+        text = "After the brutal murder of his fiancé, he turns the killers live into a living nightmare. But he made one big mistake: he underestimated the killer and soon enough the game gets turned around.";
+        description = "description5";
+        break;
+}
+
+    document.getElementById(description).innerHTML = text;
+    document.getElementById(description).style.backgroundColor = "rgba(117,114,114,0.5)";
+
+}
+
+function txtnormal(des) {
+
+let description = des;
+
+    document.getElementById(description).innerHTML = ""; 
+    document.getElementById(description).style.backgroundColor = "transparent";
+}
