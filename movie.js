3c9d085233ffb45de53fb37f97820d314187fe89
Sandy <sandra.schwarz03@gmail.com>
2022-07-31 21:02:35 +0200

    21:02 changes

--- movie.js
+++ movie.js
@@ -38,3 +38,18 @@ let description = des;
     document.getElementById(description).innerHTML = ""; 
     document.getElementById(description).style.backgroundColor = "transparent";
 }
+
+function searchSite() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('searchItems');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="list-item";
+        }
+        else {
+            x[i].style.display="none";                 
+        }
+    }
+}
